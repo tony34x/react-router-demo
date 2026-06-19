f34e67020a0801214ed8c1a2f1b1c4800f162a66
tony34x <bookerjermiah31@gmail.com>
2026-06-18 20:45:22 -0500

    feat: add NavBar component with navigation links

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import { NavLink } from 'react-router-dom';
+import './NavBar.css';
+
+function NavBar() {
+  return (
+    <nav className="menu">
+      <NavLink to="/" className="menu__link">Home</NavLink>
+      <NavLink to="/reviews" className="menu__link">Emoji Reviews</NavLink>
+      <NavLink to="/about-me" className="menu__link">About Me</NavLink>
+    </nav>
+  )
+}
+
+export default NavBar;
