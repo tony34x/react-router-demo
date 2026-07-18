559e740116ebb63d0fb7c219fcf61cee92a62d25
tony34x <bookerjermiah31@gmail.com>
2026-07-18 06:11:15 -0500

    New update

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,4 +1,5 @@
 import "./Dashboard.css";
+import { link } from "react-router-dom";
 
 function Dashboard() {
   return (
@@ -7,6 +8,7 @@ function Dashboard() {
       <p>
         The #1 Destination for Emoji Reviews on the World Wide Web Since 2020!
       </p>
+      <link to="/reviews">click here to see my latest reviews!</link>
     </div>
   );
 }
