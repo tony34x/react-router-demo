a710d70d0ddd6532c0f8f90c25d2d67a11567014
tony34x <bookerjermiah31@gmail.com>
2026-07-18 09:29:00 -0500

    fix: correct Link component usage in Dashboard and apply custom className in NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,16 +1,21 @@
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 
+
+
+const customClassName = ({ isActive }) =>
+  "menu__link" + (isActive ? " menu__link--active" : "");
+
 function NavBar() {
   return (
     <nav className="menu">
-      <NavLink to="/" className="menu__link">
+      <NavLink to="/" className={customClassName}>
         Home
       </NavLink>
-      <NavLink to="/reviews" className="menu__link">
+      <NavLink to="/reviews" className={customClassName}>
         Emoji Reviews
       </NavLink>
-      <NavLink to="/about-me" className="menu__link">
+      <NavLink to="/about-me" className={customClassName}>
         About Me
       </NavLink>
     </nav>
